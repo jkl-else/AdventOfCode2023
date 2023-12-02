28c6fda79b1c0f7ad0eb5c6c3e6f87c81784b309
Jonny Klasson <jonny.klasson@else.se>
2023-12-02 10:11:01 +0100

    Day 2 completed

--- ConsoleApp/Callendar/D02/Part1.cs
+++ ConsoleApp/Callendar/D02/Part1.cs
@@ -4,8 +4,47 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = await ReadFileLinesAsync("Input1");
+            string GetGameId(string value, out string left)
+            {
+                var stopIndex = value.IndexOf(':');
+                left = value[(stopIndex + 1)..];
+                return value["Game ".Length..stopIndex];
+            }
+            string GetFirstNumber(string value, out string left)
+            {
+                var stopIndex = value.IndexOf(' ');
+                left = value[(stopIndex + 1)..];
+                return value[..stopIndex];
+            }
+
+            var list = input.Select(x => new
+            {
+                Id = int.Parse(GetGameId(x, out var left)),
+                Games = left
+                    .Trim()
+                    .Split(';')
+                    .Select(y => y.Split(',')
+                            .Select(z => new {
+                                Number = int.Parse(GetFirstNumber(z.Trim(), out var left2)),
+                                Color = left2.Trim()
+                            }).ToList()
+                    ).ToList()
+            }).ToList();
+            var requirement = new Dictionary<string, int>
+            {
+                ["red"] = 12,
+                ["green"] = 13,
+                ["blue"] = 14
+            };
+
+            var ids = list.Where(x =>
+                x.Games.All(y =>
+                    y.All(z => !requirement.TryGetValue(z.Color, out var r) // color not required
+                        || z.Number <= r)))
+                .Select(x => x.Id)
+                .ToList();
+            return ids.Sum().ToString();
         }
     }
 }
--- ConsoleApp/Callendar/D02/Part2.cs
+++ ConsoleApp/Callendar/D02/Part2.cs
@@ -4,8 +4,70 @@
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = await ReadFileLinesAsync("Input2");
+            var list = input.Select(x => new
+            {
+                Id = int.Parse(GetGameId(x, out var left)),
+                Games = left
+                    .Trim()
+                    .Split(';')
+                    .Select(y => y.Split(',')
+                            .Select(z => new {
+                                Number = int.Parse(GetFirstNumber(z.Trim(), out var left2)),
+                                Color = left2.Trim()
+                            }).ToDictionary(z => z.Color, z => z.Number)
+                        ).ToList()
+            }).ToList();
+            var colors = new List<string>
+            {
+                "red",
+                "green",
+                "blue"
+            };
+            var result = new List<Dictionary<string, int>>();
+            foreach (var gameId in list)
+            {
+                var s = new Dictionary<string, int>();
+                foreach (var color in colors)
+                {
+                    int max = 0;
+                    foreach (var game in gameId.Games)
+                    {
+                        if (!game.TryGetValue(color, out var n))
+                            continue;
+                        if (n > max)
+                            max = n;
+                    }
+                    s.Add(color, max);
+                }
+                result.Add(s);
+            }
+
+            int res = 0;
+            foreach (var r in result)
+            {
+                int game = 1;
+                foreach (var c in r)
+                {
+                    game *= c.Value;
+                }
+                res += game;
+            }
+            return res.ToString();
+        }
+
+        private static string GetGameId(string value, out string left)
+        {
+            var stopIndex = value.IndexOf(':');
+            left = value[(stopIndex + 1)..];
+            return value["Game ".Length..stopIndex];
+        }
+
+        private static string GetFirstNumber(string value, out string left)
+        {
+            var stopIndex = value.IndexOf(' ');
+            left = value[(stopIndex + 1)..];
+            return value[..stopIndex];
         }
     }
 }
