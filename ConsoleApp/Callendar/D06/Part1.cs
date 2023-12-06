361cfa1eb5cdb70cc1d7d4b852ffec33d8cbcb67
Jonny Klasson <jonny.klasson@else.se>
2023-12-06 06:37:07 +0100

    Day 6 completed

--- ConsoleApp/Callendar/D06/Part1.cs
+++ ConsoleApp/Callendar/D06/Part1.cs
@@ -1,11 +1,37 @@
-﻿namespace ConsoleApp.Callendar.D06
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp.Callendar.D06
 {
     internal class Part1 : Part
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = await ReadFileLinesAsync("Input1");//Result = 1083852 - Result = 23501589
+            var times = Parse(input[0], "Time");
+            var distances = Parse(input[1], "Distance");
+
+            var result = times.Select((t, i) =>
+            {
+                var distance = distances[i];
+                var optionalValues = CalculateDistances(t)
+                    .Where(d => d > distance);
+                return optionalValues.Count();
+            });
+
+            return result.Aggregate(1, (current, r) => current * r)
+                .ToString();
+        }
+        private static List<int> Parse(string value, string id) => new Regex($"{id}:[ ]*(?<values>[0-9 ]*)")
+            .Match(value)
+            .Groups["values"]
+            .Value.Split(' ')
+            .Where(x => x.Length > 0)
+            .Select(int.Parse)
+            .ToList();
+        private static IEnumerable<int> CalculateDistances(int time)
+        {
+            for (var i = 0; i < time; i++)
+                yield return i * (time - i);
         }
     }
 }
--- ConsoleApp/Callendar/D06/Part2.cs
+++ ConsoleApp/Callendar/D06/Part2.cs
@@ -1,11 +1,33 @@
-﻿namespace ConsoleApp.Callendar.D06
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp.Callendar.D06
 {
     internal class Part2 : Part
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = await ReadFileLinesAsync("Input2");//Result = 23501589 - Result in: 00:00:00.6314922
+
+            var time = Parse(input[0], "Time");
+            var distance = Parse(input[1], "Distance");
+
+            var result = CalculateDistances(time)
+                .Count(x => x > distance);
+            return result.ToString();
+        }
+        private static long Parse(string value, string id)
+        {
+            var result = String.Join("", new Regex($"{id}:[ ]*(?<values>[0-9 ]*)")
+                .Match(value)
+                .Groups["values"]
+                .Value.Split(' ')
+                .Where(x => x.Length > 0));
+            return long.Parse(result);
+        }
+        private static IEnumerable<long> CalculateDistances(long time)
+        {
+            for (long i = 0; i < time; i++)
+                yield return i * (time - i);
         }
     }
 }
