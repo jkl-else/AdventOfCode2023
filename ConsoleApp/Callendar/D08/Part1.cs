1977cc1758081d1a4e5cdc74d9f84078b7180503
Jonny Klasson <jonny.klasson@else.se>
2023-12-09 08:43:50 +0100

    Completed day 8

--- ConsoleApp/Callendar/D08/Part1.cs
+++ ConsoleApp/Callendar/D08/Part1.cs
@@ -1,11 +1,43 @@
-﻿namespace ConsoleApp.Callendar.D08
+﻿using System.Collections.Frozen;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp.Callendar.D08
 {
     internal class Part1 : Part
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = await ReadFileLinesAsync("Input1");//Result = 16697 - Result in: 00:00:00.0219262
+            var reg = new Regex("^(?<start>[A-Z]{3}) = \\((?<left>[A-Z]{3}), (?<right>[A-Z]{3})\\)$");
+            var directions = input[0].ToList();
+            var data = input.Skip(2)
+                .Select(x => reg.Match(x))
+                .Select(x => new
+                {
+                    Start = x.Groups["start"].Value,
+                    Left = x.Groups["left"].Value,
+                    Right = x.Groups["right"].Value
+                }).ToFrozenDictionary(x => x.Start, x => (x.Left, x.Right));
+
+            return Move(directions, data).ToString();
+        }
+
+        private static int Move(List<char> directions, FrozenDictionary<string, (string Left, string Right)> data)
+        {
+            var pos = "AAA";
+            var steps = 0;
+            while (true)
+            {
+                foreach (var direction in directions)
+                {
+                    steps++;
+                    pos = direction == 'L'
+                        ? data[pos].Left
+                        : data[pos].Right;
+                    if (pos == "ZZZ")
+                        return steps;
+                }
+            }
         }
     }
 }
--- ConsoleApp/Callendar/D08/Part2.cs
+++ ConsoleApp/Callendar/D08/Part2.cs
@@ -1,11 +1,68 @@
-﻿namespace ConsoleApp.Callendar.D08
+﻿using System.Collections.Frozen;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp.Callendar.D08
 {
     internal class Part2 : Part
     {
         public override async Task<string> GetResultAsync()
         {
-            var input = await ReadFileLinesAsync("Test");
-            throw new NotImplementedException();
+            var input = await ReadFileLinesAsync("Input2"); //Result = 10668805667831 - Result in: 00:00:13.6460141
+            var reg = new Regex("^(?<start>[A-Z0-9]{3}) = \\((?<left>[A-Z0-9]{3}), (?<right>[A-Z0-9]{3})\\)$");
+            var dir = input[0].ToList();
+            var data = input.Skip(2)
+                .Select(x => reg.Match(x))
+                .Select(x => new
+                {
+                    Start = x.Groups["start"].Value,
+                    Left = x.Groups["left"].Value,
+                    Right = x.Groups["right"].Value
+                }).ToFrozenDictionary(x => x.Start, x => (x.Left, x.Right));
+            var positions = data.Keys.Where(x => x.EndsWith('A'))
+                .Select(x => new Position(x, data, dir))
+                .ToList();
+            long steps = 0;
+            var maxSteps = positions.Max(x => x.Steps);
+            while (true)
+            {
+                steps += maxSteps;
+                if (positions.All(x => (double)steps / x.Steps % 1 == 0))
+                    break;
+            }
+
+            return steps.ToString();
+        }
+
+        internal class Position
+        {
+            private string _pos;
+            private readonly FrozenDictionary<string, (string Left, string Right)> _map;
+            private readonly List<char> _dirs;
+            public int Steps { get; }
+            public Position(string pos, FrozenDictionary<string, (string Left, string Right)> map, List<char> dirs)
+            {
+                _pos = pos;
+                _map = map;
+                _dirs = dirs;
+                Steps = CalculateSteps();
+            }
+
+            private int CalculateSteps()
+            {
+                int steps = 0;
+                while (true)
+                {
+                    foreach (var dir in _dirs)
+                    {
+                        steps++;
+                        _pos = dir == 'L'
+                            ? _map[_pos].Left
+                            : _map[_pos].Right;
+                        if (_pos.EndsWith('Z'))
+                            return steps;
+                    }
+                }
+            }
         }
     }
 }
